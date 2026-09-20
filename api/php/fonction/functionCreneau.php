c0b12437f6f8b64ac163d9836bbe4791c4539f8c
jing752 <theo.httlr@eduge.ch>
2026-09-20 17:45:16 +0200

    New Feature Update Delete Creneau

--- api/php/fonction/functionCreneau.php
+++ api/php/fonction/functionCreneau.php
@@ -7,9 +7,9 @@ function TraiterGet()
 {
     $id = filter_input(INPUT_GET, "name", FILTER_SANITIZE_SPECIAL_CHARS);
     if (isset($id)) {
-        $data = GetAllCreneauByClasseName($id);
-        if ($data ==! false) {
-            
+        $data = GetCreneauByClasseName($id);
+        if ($data == ! false) {
+
             return [
                 "code" => HTTP_OK,
                 "data" => $data
@@ -107,7 +107,7 @@ function TraiterPost(array $body): array
         $heure_fin = $body["heure_fin"];
     }
 
-    if (!isset($body["salle"]) ) {
+    if (!isset($body["salle"])) {
         $critereRespecter = false;
         array_push($data, ["salle" => "Paramètre manquant"]);
     } else {
@@ -129,9 +129,121 @@ function TraiterPost(array $body): array
 }
 function TraiterPut(array $body, string $token): array
 {
-    return [
-        "code" => HTTP_OK,
-        "data" => []
-    ];
+    $classe_id = 0;
+    $cours_id = 0;
+    $jour = "";
+    $heure_debut = "";
+    $heure_fin = "";
+    $salle = 0;
+    $critereRespecter = true;
+    $code = HTTP_CREATED;
+    $data = [];
+    //Verification du champ classe
+    if (!isset($body["classe"])) {
+        $critereRespecter = false;
+        array_push($data, ["classe" => "Paramètre manquant"]);
+    } else {
+        if (!GetClasseByName($body["classe"])) {
+            $critereRespecter = false;
+            array_push($data, ["classe" => "Veillez rentré une classe qui existe"]);
+        } else {
+            $classe_id = GetClasseByName($body["classe"])["id"];
+        }
+    }
+
+    //Verification du champ cours
+    if (!isset($body["cours"])) {
+        $critereRespecter = false;
+        array_push($data, ["cours" => "Paramètre manquant"]);
+    } else {
+        if (!GetCoursByName($body["cours"])) {
+            $critereRespecter = false;
+            array_push($data, ["cours" => "Veillez rentré une classe qui existe"]);
+        } else {
+            $cours_id = GetCoursByName($body["cours"])["id"];
+        }
+    }
+
+    if (!isset($body["jour"])) {
+        $critereRespecter = false;
+        array_push($data, ["jour" => "Paramètre manquant"]);
+    } else {
+        $checkJour = false;
+        $joursArray = [
+            "lundi",
+            "mardi",
+            "mercredi",
+            "jeudi",
+            "vendredi"
+        ];
+
+        foreach ($joursArray as $value) {
+            if ($value == $body["jour"]) {
+                $checkJour = true;
+            }
+        }
+        if ($checkJour) {
+            $jour = $body["jour"];
+        } else {
+            $critereRespecter = false;
+            array_push($data, ["jour" => "Veuillez rentrés un jour de la semaine en minuscule"]);
+        }
+    }
+
+    if (!isset($body["heure_debut"])) {
+        $critereRespecter = false;
+        array_push($data, ["heure_debut" => "Paramètre manquant"]);
+    } else {
+        $heure_debut = $body["heure_debut"];
+    }
+
+    if (!isset($body["heure_fin"])) {
+        $critereRespecter = false;
+        array_push($data, ["heure_fin" => "Paramètre manquant"]);
+    } else {
+        $heure_fin = $body["heure_fin"];
+    }
+
+    if (!isset($body["salle"])) {
+        $critereRespecter = false;
+        array_push($data, ["salle" => "Paramètre manquant"]);
+    } else {
+        $salle = "Salle : " . $body["salle"];
+    }
+
+    if ($critereRespecter) {
+        AddCreneau($classe_id, $cours_id, $jour, $heure_debut, $heure_fin, $salle);
+        return [
+            "code" => $code,
+            "data" => ["Succes" => "Ajout avec Succes"]
+        ];
+    } else {
+        return [
+            "code" => $code,
+            "data" => $data
+        ];
+    }
 }
-function TraiterDelete($body) {}
+function TraiterDelete()
+{
+    $id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+    if (is_int($id)) {
+        if (!GetCreneauById($id)) {
+            return [
+                "code" => HTTP_BAS_REQUEST,
+                "data" => ["id" => "Cette id n'existe pas"]
+            ];
+        } else {
+            DeleteCreneauById($id);
+            return [
+                "code" => HTTP_NO_CONTENT,
+                "data" => ["Delete" => "Suppresion reussi avec succee"]
+            ];
+        }
+    } else {
+        return [
+            "code" => HTTP_BAS_REQUEST,
+            "data" => ["id" => "Cette id doit etre un entier"]
+        ];
+    }
+}
--- api/php/sql/creneau.php
+++ api/php/sql/creneau.php
@@ -9,7 +9,7 @@ function GetAllCreneau(): array
     return $statement->fetchAll();
 }
 
-function GetAllCreneauByClasseName(string $name): array
+function GetCreneauByClasseName(string $name): array
 {
     $params = [
         ":name" => $name
@@ -45,6 +45,55 @@ function AddCreneau(int $classe_id, int $cours_id, string $jour, string $heure_d
     $statement = db()->prepare(
         "INSERT INTO `creneaux` (`id`, `classe_id`, `cours_id`, `jour`, `heure_debut`, `heure_fin`, `salle`)
         VALUES (NULL, :classe_id, :cours_id, :jour, :heure_debut, :heure_fin, :salle);"
+    );
+
+    $statement->execute($params);
+}
+
+function GetCreneauById(int $id) : array|false
+{
+    $params = [
+        ":id" => $id
+    ];
+    $statement = db()->prepare(
+        'SELECT * FROM creneaux WHERE creneaux.id = :id'
+    );
+    $statement->execute($params);
+    return $statement->fetchAll();
+}
+
+function DeleteCreneauById(int $id)
+{
+    $params = [
+        ":id" => $id
+    ];
+    $statement = db()->prepare(
+        'DELETE FROM creneaux WHERE creneaux.id = :id'
+    );
+    $statement->execute($params);
+}
+
+function updateCreneau(int $id, int $classe_id, int$cours_id, string $jour, string$heure_debut, string $heure_fin, string$salle)
+{
+    $params = [
+        ":id" => $id,
+        ":classe_id" => $classe_id,
+        ":cours_id" => $cours_id,
+        ":jour" => $jour,
+        ":heure_debut" => $heure_debut,
+        ":heure_fin" => $heure_fin,
+        ":salle" => $salle
+    ];
+
+    $statement = db()->prepare(
+        "UPDATE `creneaux` 
+         SET `classe_id` = :classe_id, 
+             `cours_id` = :cours_id, 
+             `jour` = :jour, 
+             `heure_debut` = :heure_debut, 
+             `heure_fin` = :heure_fin, 
+             `salle` = :salle 
+         WHERE `id` = :id;"
     );
 
     $statement->execute($params);
